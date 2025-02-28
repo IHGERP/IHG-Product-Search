06ae3128e8ce730af7546ae1ef1c238c215b37e4
Tridots Tech <info@tridotstech.com>
2025-02-28 13:26:05 +0000

    refactor:Enabling login using username

--- igh_search/igh_search/api.py
+++ igh_search/igh_search/api.py
@@ -152,9 +152,9 @@ def get_user_credentials(email, pwd):
         login_manager.post_login()
     except frappe.exceptions.AuthenticationError:
         return {"key": 0, "message": "Incorrect Username or Password"}
-    frappe.log_error("session_user",frappe.session.user)
+    # frappe.log_error("session_user",frappe.session.user)
+    user = frappe.get_doc("User", frappe.session.user)
     frappe.set_user("Administrator")
-    user = frappe.get_doc("User", email)
     if not user.api_key:
         api_key = frappe.generate_hash(length=15)
         user.api_key = api_key
